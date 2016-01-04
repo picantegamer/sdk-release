adbe6c658ae20e9361b7ca8a95eda3cbeecbe63b
Subramanian <subramam@amazon.com>
2016-01-04 13:00:27 -0800

    Initial commit

---
+++ core-module-1/src/main/java/com/aws/java/sdk/Foo.java
@@ -0,0 +1,8 @@
+package com.aws.java.sdk;
+
+public class Foo {
+
+    public static void main() {
+        System.out.println("hello from core-module-1");
+    }
+}
---
+++ highlevel-library-2/src/main/java/com/aws/java/sdk/Foo.java
@@ -0,0 +1,8 @@
+package com.aws.java.sdk;
+
+public class Foo {
+
+    public static void main() {
+        System.out.println("hello from highlevel-library-2");
+    }
+}
